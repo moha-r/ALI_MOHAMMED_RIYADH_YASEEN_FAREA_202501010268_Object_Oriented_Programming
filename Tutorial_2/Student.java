7dc71eb7ce33fecf1325ba595b03ce1316c8e0cf
ALI MOHAMMED RIYADH <162194567+moha-r@users.noreply.github.com>
2026-06-09 04:10:46 +0000

    tutorial_2

---
+++ Tutorial_2/Student.java
@@ -0,0 +1,16 @@
+//Attributs
+class Student{
+    String name;
+    int age; 
+    double apa; 
+
+
+//constuctor
+Student(String studentName, int,)
+}
+
+
+public void displayInfo() {
+	// TODO Auto-generated method stub
+	throw new UnsupportedOperationException("Unimplemented method 'displayInfo'");
+}
---
+++ Tutorial_2/main.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args) {
+        // coding starts here; 
+        Student s1 = new Student("MOHAMMED", 18, "RIYADH");
+
+        s1.displayInfo();
+        System.out.println(s1);
+
+    }
+}
