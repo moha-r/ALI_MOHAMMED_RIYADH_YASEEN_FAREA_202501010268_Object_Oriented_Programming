b791cf5e122cbc0ef5cdd19e2ecd177006fecc4a
ALI MOHAMMED RIYADH <mohammed.almusni@gmail.com>
2026-08-11 01:22:20 +0800

    feat: complete tutorial 6 inheritance

---
+++ tutorial_6/Lecturer.java
@@ -0,0 +1,15 @@
+public class Lecturer extends Employee {
+    private String subject;
+    private String department;
+
+    public Lecturer(String id, String name, String subject, String department) {
+        super(id, name);
+        this.subject = subject;
+        this.department = department;
+    }
+
+    public void displaySubject() {
+        System.out.println("Subject     : " + subject);
+        System.out.println("Department  : " + department);
+    }
+}
