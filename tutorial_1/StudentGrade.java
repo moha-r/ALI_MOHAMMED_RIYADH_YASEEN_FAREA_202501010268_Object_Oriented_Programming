82214e0f29cbd2183e55a8621984bb3140a8775e
ALI MOHAMMED RIYADH <mohammed.almusni@gmail.com>
2026-08-11 01:21:19 +0800

    feat: complete tutorial 1 basics

---
+++ tutorial_1/StudentGrade.java
@@ -0,0 +1,23 @@
+public class StudentGrade {
+    public static void main(String[] args) {
+        String studentName = "Ali";
+        int mark = 85;
+        char grade;
+
+        if (mark >= 80) {
+            grade = 'A';
+        } else if (mark >= 70) {
+            grade = 'B';
+        } else if (mark >= 60) {
+            grade = 'C';
+        } else if (mark >= 50) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("Student: " + studentName);
+        System.out.println("Mark: " + mark);
+        System.out.println("Grade: " + grade);
+    }
+}
